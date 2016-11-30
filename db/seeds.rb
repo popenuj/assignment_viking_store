9072def929f13c100cfe20fcebe53aa8bb2d955f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-29 19:08:34 -0500

    Implement seeding module

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+# generate users
+# generate users' credit card(s)
+# generate shopping cart
 #
-# Examples:
+# generate country
+# generate state
+# generate address
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# generate product categories
+# generate product
+
+
+module Seeder
+
+  class << self
+
+    def generate_users(n)
+      n.times do
+        User.create(name: fake_name,
+                    email: fake_email,
+                    phone_number: fake_phone_number)
+      end
+    end
+
+    def generate_credit_cards(owners)
+      number_of_owners = owners.length
+      (number_of_owners/ (random_number(2, 1))).times do
+        owner = owners.sample
+        CreditCard.create(user_id: owner.id,
+                          expiration_date: fake_expiry_date,
+                          name: owner.name,
+                          number: fake_credit_card_number)
+      end
+    end
+
+    private
+
+      def fake_name
+        Faker::Name.name
+      end
+
+      def fake_email
+        Faker::Internet.email
+      end
+
+      def fake_phone_number
+        Faker::PhoneNumber.phone_number
+      end
+
+      def fake_expiry_date
+        Faker::Business.credit_card_expiry_date
+      end
+
+      def fake_credit_card_number
+        Faker::Business.credit_card_number
+      end
+
+      def random_number(max, min = 0)
+        Random.new.rand(max) + min
+      end
+  end
+end
